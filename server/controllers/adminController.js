e2aca345de2e3ac43a4bec20d0b60f3e54a6efef
Chibytez <aniakuchibuike@gmail.com>
2018-09-09 07:15:33 +0200

    admin delete order

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -30,8 +30,8 @@ export const getAllOrders = (req, res) => {
     price: req.body.price
   });
 };
-
-export const deleteAnOrder = (req, res) => {
+//delete item
+export const deleteAnOrder = (req, res) =>{
   let orderId = req.params.Id;
   let currentOrder = globalArrayHolder.filter(e =>e.Id == orderId)[0];
 res.status(200).json({
