f15b0716d028ae058af4b0e898e6177765e79749
Chibytez <aniakuchibuike@gmail.com>
2018-09-12 22:36:38 +0200

    user can cancel his order

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,9 +11,7 @@ const userRoutes = (app) => {
 
     app.get("/api/v1/orders", getAllOrders);
     app.get("/api/v1/orders/:id", getSingleOrders);
-app.delete("/api/v1/allOrders/orderId", cancelAnOrder);
-
-
+    app.delete("/api/v1/orders/:id", cancelAnOrder);
 
 };
 
