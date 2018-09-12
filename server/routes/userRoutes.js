3e22e8ddd8bf02159f96f9daa6ef372ef136b5d8
Chibytez <aniakuchibuike@gmail.com>
2018-09-12 22:29:17 +0200

    edited the users get item by id

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,7 +10,7 @@ const userRoutes = (app) => {
 
 
     app.get("/api/v1/orders", getAllOrders);
-app.get("/api/v1/orders/orderId", getSingleOrders);
+    app.get("/api/v1/orders/:id", getSingleOrders);
 app.delete("/api/v1/allOrders/orderId", cancelAnOrder);
 
 
