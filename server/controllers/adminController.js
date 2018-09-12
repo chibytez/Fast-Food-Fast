37ae694d653a875284efb0e7112dad2119d3f7fd
Chibytez <aniakuchibuike@gmail.com>
2018-09-12 22:55:21 +0200

    admin can fetch specific food order

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -22,14 +22,18 @@ res.status(200).json({
   order: globalArrayHolder
 });
 };
-  export const getSingleOrders = (req, res) => {
-    let orderId = req.params.Id;
-    let currentOrder = globalArrayHolder.filter(e=> e.Id == orderId)[0];
-    res.status(200).json({
-      message: "Rice And Chicken",
-      order: globalArrayHolder
-    });
-  };
+
+export const getSingleOrders = (req, res) => {
+  let id = parseInt(req.params.id);
+  let currentOrder = globalArrayHolder.filter(e=> e.id == id)[0];
+if (!currentOrder){
+  res.status(404);
+}else{
+  res.status(200).json({
+    order:currentOrder
+  });
+};
+};
 
   export const editAnOrder = (req, res) => {
     let orderId = req.params.Id;
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -10,7 +10,7 @@ import { getAllOrders,
 
 const adminRoutes = app => {
     app.get("/api/v1/allOrders", getAllOrders);
-    app.get("/api/v1/allOrders/orderId", getSingleOrders);
+    app.get("/api/v1/allOrders/:id", getSingleOrders);
     app.put("/api/v1/allOrders/orderId", editAnOrder);
     app.delete("/api/v1/allOrders/orderId", deleteAnOrder);
     app.post("/api/v1/allOrders/orderId", createNewItem);
