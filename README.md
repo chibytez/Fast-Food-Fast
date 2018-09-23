217a0bc803382d856908ff46639e706e1cc487f2
Chibytez <aniakuchibuike@gmail.com>
2018-09-23 14:19:56 +0200

    tested my endpoints

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-  [![Build Status](https://travis-ci.com/chibytez/Fast-Food-Fast.svg?branch=develop)]
-  (https://travis-ci.com/chibytez/Fast-Food-Fast)
+  [![Build Status](https://travis-ci.com/chibytez/Fast-Food-Fast.svg?branch=develop)](https://travis-ci.com/chibytez/Fast-Food-Fast)
   [![Coverage Status](https://coveralls.io/repos/github/chibytez/Fast-Food-Fast/badge.svg?branch=develop)](https://coveralls.io/github/chibytez/Fast-Food-Fast?branch=develop)
-
+[![Maintainability](https://api.codeclimate.com/v1/badges/077a4de5979ad55bf126/maintainability)](https://codeclimate.com/github/chibytez/Fast-Food-Fast/maintainability")
   
 
 ## Fast-Food-Fast
---
+++ server/test/adminRoutes.spec.js
@@ -0,0 +1,121 @@
+import chai from 'chai';
+
+ import chaiHttp from 'chai-http';
+
+import app from '../../app';
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+
+describe('ADMIN ROUTES',() =>{
+    //Get all orders
+it('Should Get All orders',(done)=>{
+    chai.request(app)
+    .get("/api/v1/allOrders")
+    .then((res) =>{
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('array')
+    })
+    done();
+});
+
+//Get Single order
+it('Should Get All orders',(done)=>{
+    chai.request(app)
+    .get("/api/v1/allOrders/:id")
+    .then((res) =>{
+        expect(res).to.have.status(200);
+        expect(res).to.be.an('array')
+    }) 
+    done();
+});
+
+//Get -invalid path
+it('should return not found',(done)=>{
+    chai.request(app)
+    .get('/INVALID_PATH')
+    .then((res) =>{
+        throw new Error('path exists!');
+    })
+    .catch((err)=>{
+        expect(err).to.have.status(404);
+    });
+    done(); 
+});
+
+//Post- Add a new order
+it('should add a new order', (done)=>{
+chai.request(app)
+.post("/api/v1/allOrders")
+.then((res) =>{
+    expect(res).to.have.status(201);
+    expect(res).to.be.an('array');
+}) 
+done();
+});
+
+//Post - Bad Request
+it('should return bad request', (done)=>{
+chai.request(app)
+.post('/INVALID_PATH')
+.then((res)=>{
+    throw new Error ('Invalid content');
+})
+.catch((err)=>{
+    expect(err).to.have.status(400);
+})
+done();
+});
+
+//Put Edit An Order
+it('should edit an order', (done)=>{
+chai.request(app)
+.put("/api/v1/allOrders/:id")
+.then((res)=>{
+  expect(res).to.have.status(200);
+expect(res).to.be.an('array');
+});
+done();
+});
+
+// bad put request
+it('Should show an error message when order do not exist',(done)=>{
+    chai.request(app)
+    .put('/INVALID_PATH')
+    .then((res)=>{
+        throw new Error ('Invalid content');
+    })
+    .catch((err)=>{
+        expect(err).to.have.status(400);
+    })
+    done();
+    }); 
+
+    //delete an order
+    it('should delete an item', (done)=>{
+            chai.request(app)
+            .delete("/api/v1/allOrders/:id")
+            .then((res)=>{
+              expect(res).to.have.status(201);
+            expect(res).to.be.an('array');
+            });
+            done();
+            });
+
+            //bad delete request
+            it('Should show an error message when order do not exist',(done)=>{
+                chai.request(app)
+                .delete('/INVALID_PATH')
+                .then((res)=>{
+                    throw new Error ('Invalid content');
+                })
+                .catch((err)=>{
+                    expect(err).to.have.status(404);
+                })
+                done();
+                }); 
+            
+
+});
