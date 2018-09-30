f2bb5d2ea112484d359bd893ece5871a6cf90e42
Chibytez <aniakuchibuike@gmail.com>
2018-09-30 17:27:18 +0100

    Updating the status of an order
    feature- Admin updating the statu of an order

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -16,3 +16,27 @@ export const getSpecificOrder = (req, res) => {
   dbResults(sql, req.userInfo, res);
 
 };
+
+export const updateOrderStatus = (req, res) => {
+  const id = parseInt(req.params.orderId, 10);
+  const { action } = req.params;
+  let sql = '';
+  switch (action) {
+    case 'new':
+      sql = `UPDATE orders SET status=${1} WHERE id=${id} RETURNING *`;
+      break;
+    case 'processing':
+      sql = `UPDATE orders SET status=${2} WHERE id=${id} RETURNING *`;
+      break;
+    case 'cancelled':
+      sql = `UPDATE orders SET status=${3} WHERE id=${id} RETURNING *`;
+      break;
+    case 'complete':
+      sql = `UPDATE orders SET status=${3} WHERE id=${id} RETURNING *`;
+      break;
+    default:
+      sql = `UPDATE orders SET status=${0} WHERE id=${id} RETURNING *`;
+      break;
+  }
+  dbResults(sql, req.userInfo, res);
+};
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -2,6 +2,7 @@
 import {
   getAllOrders,
   getSpecificOrder,
+  updateOrderStatus,
 } from '../controllers/adminController';
 import validate from '../helper/validateAdmin';
 
@@ -9,7 +10,7 @@ import validate from '../helper/validateAdmin';
 const adminRoutes = (app) => {
   app.get('/api/v1/allOrders', getAllOrders);
   app.get('/api/v1/allOrders/:id', getSpecificOrder);
-  app.put('/api/v1/allOrders/:id', validate, editAnOrder);
+  app.put('/api/v1/allOrders/:id', validate, updateOrderStatus);
   app.delete('/api/v1/allOrders/:id', validate, deleteAnOrder);
   app.post('/api/v1/allOrders', validate, createNewOrder);
 };
