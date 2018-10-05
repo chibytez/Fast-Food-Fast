c81f9c1580c54f2928687442d4b56976926cbcc1
Chibytez <aniakuchibuike@gmail.com>
2018-10-05 12:33:12 +0100

    an updated

--- server/helper/utilities.js
+++ server/helper/utilities.js
@@ -2,7 +2,7 @@ import db from '../models/database';
 
 export const dbResults = (sql, user, res) => {
   db.query(sql, (err, result) => {
-    res.status(200)
+    return res.status(200)
       .json({
         user,
         result: result.rows,
