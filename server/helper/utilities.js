c7a031be7d6a3b12e89d713afce006b1820a8cee
Chibytez <aniakuchibuike@gmail.com>
2018-12-03 15:37:21 +0100

    implementing feedbacks

--- server/helper/utilities.js
+++ server/helper/utilities.js
@@ -2,10 +2,10 @@ import db from '../models/database';
 
 export const dbResults = (sql, user, res) => {
   db.query(sql, (err, result) => {
-    return res.status(200)
+     res.status(200)
       .json({
         user,
-        result: result.rows,
+       result: result.rows,
       });
   });
 };
